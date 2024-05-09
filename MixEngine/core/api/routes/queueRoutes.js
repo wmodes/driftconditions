cca2412dd77a564487ba03c0c856b5e34e89f07a
Wes Modes <wmodes@gmail.com>
2024-05-08 21:14:14 -0700

    liquidsoap config

--- MixEngine/core/api/routes/queueRoutes.js
+++ MixEngine/core/api/routes/queueRoutes.js
@@ -22,7 +22,7 @@ router.get('/nextmix', async (req, res) => {
   try {
     const mixObj = await getNextMixFromQueue();
     if (mixObj) {
-      // await markMixAsPlayed(mixObj.mixID);
+      await markMixAsPlayed(mixObj.mixID);
       logger.info(`queueRoutes:/nextmix: Sending mix: ${mixObj.filename}`);
       res.send(mixObj.filename);
     } else {
