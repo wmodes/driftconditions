ada9879590bca0772ea57fbf766a41caf238546b
Wes Modes <wmodes@gmail.com>
2024-05-10 08:09:41 -0700

    homepage reorg

--- AdminClient/src/pages/Homepage.js
+++ AdminClient/src/pages/Homepage.js
@@ -4,9 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchQueuePlaylist } from '../store/queueSlice';
 import { renderPlaylist } from '../utils/queueUtils';
-// feather icons
 import FeatherIcon from 'feather-icons-react';
-// tracery text
 import { generateRandomTexts } from '../utils/textUtils'; 
 
 const Homepage = () => {
@@ -44,8 +42,25 @@ const Homepage = () => {
     <div className="profile-edit-wrapper">
       <div className="homepage-box-wrapper">
         <div className="homepage-box">
+
           <div className="column1">
-            <h2 className='title'>
+
+            <div className='player-wrapper mb-8'>
+              <h2 className='title'>
+              <FeatherIcon icon="volume-2" />&nbsp;listen</h2>
+              <div className="player text-center">
+                <p>Listen to { projectName } live. The broadcast is assembled live, on-the-fly, and will never be heard exactly the same again.</p>
+                <div className="flex justify-center w-full">
+                  <audio controls className="inline-block">
+                    <source src="https://driftconditions.org:8000/stream" type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </div>
+              </div>
+            </div>
+
+            <div className='narrative-wrapper'>
+              <h2 className='title'>
               <FeatherIcon icon="radio" />&nbsp;tune in</h2>
               <div className="text">
                 <p key={0} dangerouslySetInnerHTML={createMarkup(generatedText[0])}></p> 
@@ -53,35 +68,28 @@ const Homepage = () => {
                 <p className="pullquote" key={2} dangerouslySetInnerHTML={createMarkup(generatedText[2])}></p>
                 <p key={3} dangerouslySetInnerHTML={createMarkup(generatedText[3])}></p> 
               </div>
-            </div>
-            <div className="column2">
-              <div className='player-wrapper'>
-                <h2 className='title'>
-                <FeatherIcon icon="volume-2" />&nbsp;listen</h2>
-                <div className="player text-center">
-                  <p>Listen to { projectName } live.</p>
-                  <div class="flex justify-center w-full">
-                    <audio controls className="inline-block">
-                      <source src="https://driftconditions.org:8000/stream" type="audio/mpeg" />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </div>
-              </div>
-              <div className='playlist-wrapper mt-4'>
-                <h2 className='title'>
-                <FeatherIcon icon="volume-2" />&nbsp;playlist</h2>
-                <div className="playlist text-center">
-                  {/* put playlist here updated every minute or so */}
-                  <div className="playlist-wrapper">
-                    {renderPlaylist(playlist)}
-                  </div>
+            </div> {/* end narrative-wrapper */}
+
+          </div> {/* end column1 */}
+
+          <div className="column2">
+
+            <div className='playlist-wrapper mt-4'>
+              <h2 className='title'>
+              <FeatherIcon icon="list" />&nbsp;playlist</h2>
+              <div className="playlist text-center">
+                {/* put playlist here updated every minute or so */}
+                <div className="playlist-wrapper">
+                  {renderPlaylist(playlist)}
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+          </div> {/* end column2 */}
+
+        </div> 
+      </div> 
+    </div> 
   );
 };
 
