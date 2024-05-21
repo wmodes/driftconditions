47bb339cb195dea11bd1816affb2014d8365001b
Wes Modes <wmodes@gmail.com>
2024-05-21 08:26:31 -0700

    homepage tqeaks

--- AdminClient/src/utils/textUtils.js
+++ AdminClient/src/utils/textUtils.js
@@ -1,75 +1,85 @@
 // textUtils.js - utility functions for generating random texts and project name
 
-// Import the config object from the config.js file
-import config from '../config/config';
-// pull variables from the config object
-const projectNames = config.app.projectNames;
-
 var tracery = require('tracery-grammar');
 
 var grammarDefinition = {
-    "origin": [
-        "#assertion#"
-    ],
-    "intro": [
-      "%projectName% is an online radio station that captures #evocativeNounPhrase# in #evocativeNounPhrase#."
-    ],
-    "assertion": [
-        "#thingsWeWillFind.capitalize#, #thingsWeWillFind#, and #thingsWeWillFind# weave #thingsWeWillFind# that draws listeners into #evocativeNounPhrase#.",
-        "Inspired by the unpredictability of real-world radio broadcast, %projectName% explores the boundaries between intention and happenstance, inviting listeners to eavesdrop on #evocativeNounPhrase#.",
-        "With each new listening experience, %projectName% offers #evocativeNounPhrase#.",
-        "%projectName% whispers its tales as secrets shared under the cloak of night, #evocativeNounPhrase#.",
-        "It's told not with the clarity of daylight but with the mystery of shadows, where sounds blend and tales intertwine like conversations on a long, late-night journey with a close companion.",
-        "Stories come through in #thingsWeWillFind#, #thingsWeWillFind#, #thingsWeWillFind#, #thingsWeWillFind#.",
-        "%projectName% invites you to lean closer, to become part of #evocativeNounPhrase#, #evocativeNounPhrase#, all wrapped in #evocativeNounPhrase#.",
-        "%projectName% blends #evocativeNounPhrase# with #evocativeNounPhrase#, crafting #evocativeNounPhrase#.",
-        "This online audio experience weaves #evocativeNounPhrase#, drawing listeners into #evocativeNounPhrase#.",
-        "Each session offers #evocativeNounPhrase#, exploring #evocativeNounPhrase#.",
-        "#thingsWeWillFind#, #thingsWeWillFind#, and #thingsWeWillFind# weave #thingsWeWillFind# that draws listeners into #evocativeNounPhrase#.",
-        "Inspired by the unpredictability of real-world radio interference, %projectName% explores the boundaries between intention and happenstance, inviting listeners to eavesdrop on #evocativeNounPhrase#.",
-        "With each new listening session, %projectName% offers #evocativeNounPhrase#."
-    ],
-    "thingsWeWillFind": [
-        "overlapping fragmented stories",
-        "a distorted broadcast",
-        "a larger narrative",
-        "ambient sounds",
-        "fragmented pieces",
-        "mysterious crosstalk",
-        "an overheard conversation",
-        "a vivid sonic collage",
-        "interwoven narratives",
-        "a disjointed transmission",
-        "an unfolding tableau",
-        "whispers in the static",
-        "a tapestry of echoes",
-        "echoes of lost voices",
-        "a mosaic of memories",
-        "shadows of untold stories",
-        "a symphony of dissonance",
-        "veiled whispers"        
-    ],
-    "plainENP": [
-        "a fresh journey through its evocative auditory landscape",
-        "a hidden world of voices and atmospheres unconstrained by traditional narrative structures",
-        "a tapestry of serendipitous encounters",
-        "a unique journey through its evocative soundscape",
-        "a world where intention and chance converge",
-        "a realm beyond traditional narratives",
-        "an immersive and unpredictable listening experience",
-        "an immersive stream of stories, sounds, and crosstalk",
-        "an intimate exchange between the listener and the vast, unseen world",
-        "an unfolding mystery",
-        "the creativity of code-generated audio",
-        "the chaos and serendipity of late-night radio tuning",
-        "the liminality of an endless night drive",
-        "the unpredictability of late-night radio",
-        "the warmth of voice and the chill of the unknown",
-        "an uncanny audio stream generated on the fly by code"
-    ],
-    "evocativeNounPhrase": [
-      "#plainENP#", "<b>#plainENP#</b>"
-    ]
+  "origin": [
+    "#assertion#"
+  ],
+  "intro": [
+    "%projectName% is an online radio station that captures #evocativeNounPhrase# in #evocativeNounPhrase#."
+  ],
+  "assertion": [
+    "#thingsWeWillFind.capitalize#, #thingsWeWillFind#, and #thingsWeWillFind# weave #thingsWeWillFind# that draws listeners into #evocativeNounPhrase#.",
+    "Inspired by the unpredictability of real-world radio broadcast, %projectName% explores the boundaries between intention and happenstance, inviting listeners to eavesdrop on #evocativeNounPhrase#.",
+    "With each new listening experience, %projectName% offers #evocativeNounPhrase#.",
+    "%projectName% whispers its tales as secrets shared under the cloak of night, #evocativeNounPhrase#.",
+    "It's told not with the clarity of daylight but with the mystery of shadows, where sounds blend and tales intertwine like conversations on a long, late-night journey with a close companion.",
+    "Stories come through in #thingsWeWillFind#, #thingsWeWillFind#, #thingsWeWillFind#, #thingsWeWillFind#.",
+    "%projectName% invites you to lean closer, to become part of #evocativeNounPhrase#, #evocativeNounPhrase#, all wrapped in #evocativeNounPhrase#.",
+    "%projectName% blends #evocativeNounPhrase# with #evocativeNounPhrase#, crafting #evocativeNounPhrase#.",
+    "This online audio experience weaves #evocativeNounPhrase#, drawing listeners into #evocativeNounPhrase#.",
+    "Each session offers #evocativeNounPhrase#, exploring #evocativeNounPhrase#.",
+    "#thingsWeWillFind#, #thingsWeWillFind#, and #thingsWeWillFind# weave #thingsWeWillFind# that draws listeners into #evocativeNounPhrase#.",
+    "Inspired by the unpredictability of real-world radio interference, %projectName% explores the boundaries between intention and happenstance, inviting listeners to eavesdrop on #evocativeNounPhrase#.",
+    "With each new listening session, %projectName% offers #evocativeNounPhrase#."
+  ],
+  "thingsWeWillFind": [
+    "overlapping fragmented stories",
+    "a distorted broadcast",
+    "a larger narrative",
+    "ambient sounds",
+    "fragmented pieces",
+    "mysterious crosstalk",
+    "an overheard conversation",
+    "a vivid sonic collage",
+    "interwoven narratives",
+    "a disjointed transmission",
+    "an unfolding tableau",
+    "whispers in the static",
+    "a tapestry of echoes",
+    "echoes of lost voices",
+    "a mosaic of memories",
+    "shadows of untold stories",
+    "a symphony of dissonance",
+    "veiled whispers"        
+  ],
+  "plainENP": [
+    "a fresh journey through its evocative auditory landscape",
+    "a hidden world of voices and atmospheres unconstrained by traditional narrative structures",
+    "a tapestry of serendipitous encounters",
+    "a unique journey through its evocative soundscape",
+    "a world where intention and chance converge",
+    "a realm beyond traditional narratives",
+    "an immersive and unpredictable listening experience",
+    "an immersive stream of stories, sounds, and crosstalk",
+    "an intimate exchange between the listener and the vast, unseen world",
+    "an unfolding mystery",
+    "the creativity of code-generated audio",
+    "the chaos and serendipity of late-night radio tuning",
+    "the liminality of an endless night drive",
+    "the unpredictability of late-night radio",
+    "the warmth of voice and the chill of the unknown",
+    "an uncanny audio stream generated on the fly by code"
+  ],
+  "evocativeNounPhrase": [
+    "#plainENP#", "<b>#plainENP#</b>"
+  ],
+  "projectName": [
+    "#projNoun##projNoun#", "#projAdj##projNoun#", "#projAdj##projPlural#", "#projNoun##projPlural#"
+  ],
+  "projNoun": [
+    "Static", "Drift", "Radio", "Nocturne", "Event", "Interference", "Dusk", "Frequency", "Elegy", "Diaspora", "Project", "Aether", "Flow", "Schema", "Protocol", "Shutdown", "Matrix", "Feedstock", "Fidelity", "Harmony", "Dissonance", "Resonance", "Feedback", "Modulation", "Amplitude", "Noise", "Tone", "Timbre", "Pitch", "Rhythm", "Dynamics", "Articulation", "Phrasing",
+    "Control", "Process", "System", "Equipment", "Operation", "Variable", "Instrumentation", "Utility", "Procedure", "Safety", "Analysis", "Catalyst", "Drawing", "Standard", "Specification", "Manual", "Design"
+  ],
+  "projPlural": [
+    "Conditions", "Variations", "Matrices", "Schemas", "Protocols", "Fidelities", "Frequencies",
+    "Processes", "Systems", "Operations", "Variables", "Utilities", "Procedures", "Analyses", "Catalysts", "Standards", "Specifications"
+  ],
+  "projAdj": [
+    "Static", "Toxic", "Halcyon", "Safety", "Radio", "Drift", "Dusk",
+    "General", "Dynamic", "Licensed", "NonLicensed", "Auxiliary", "Analytical"
+  ]
 }
 
 var grammar = tracery.createGrammar(grammarDefinition);
@@ -113,8 +123,10 @@ export const generateRandomTexts = (projectName) => {
 export const getProjectName = () => {
   let projectName = sessionStorage.getItem('projectName');
   if (!projectName) {
-    projectName = projectNames[Math.floor(Math.random() * projectNames.length)];
+    // projectName = projectNames[Math.floor(Math.random() * projectNames.length)];
+    projectName = grammar.flatten('#projectName#')
     sessionStorage.setItem('projectName', projectName);
   }
+  // console.log('projectName:', projectName);
   return projectName;
 };
