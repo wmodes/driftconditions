fc4583ff8221660d0cb898b6fe8871ba634eae2b
Wes Modes <wmodes@gmail.com>
2024-06-02 16:47:57 -0700

    service file/scripts twiddless

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+clear
+echo "# SYTEM STATUS #"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
 # Restart Caddy service
-echo "# CADDY SERVICE"
+echo "## CADDY SERVICE ##"
 sudo systemctl status caddy -n 0 --no-pager
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart adminserver.service
-echo "# ADMINSERVER SERVICE"
+echo "## ADMINSERVER SERVICE ##"
 sudo systemctl status adminserver.service -n 0 --no-pager
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart mixengine.service
-echo "# MIXENGINE SERVICE"
+echo "## MIXENGINE SERVICE ##"
 sudo systemctl status mixengine.service -n 0 --no-pager
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart icecast
-echo "# ICECAST SERVICE"
+echo "## ICECAST SERVICE ##"
 sudo systemctl status icecast2 -n 0 --no-pager
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart liquidsoap
-echo
+echo "## LIQUIDSOAP SERVICE ##"
 sudo systemctl status liquidsoap -n 0 --no-pager
