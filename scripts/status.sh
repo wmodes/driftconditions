721df7fd00250ae609fb7c9737f94893ba4c7060
Wes Modes <wmodes@gmail.com>
2024-06-02 16:53:44 -0700

    service file/scripts twiddles

--- scripts/status.sh
+++ scripts/status.sh
@@ -6,25 +6,25 @@ printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart Caddy service
 echo "## CADDY SERVICE ##"
-sudo systemctl status caddy -n 0 --no-pager
+sudo systemctl status caddy -n 0 --no-pager --color=always
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart adminserver.service
 echo "## ADMINSERVER SERVICE ##"
-sudo systemctl status adminserver.service -n 0 --no-pager
+sudo systemctl status adminserver.service -n 0 --no-pager --color=always
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart mixengine.service
 echo "## MIXENGINE SERVICE ##"
-sudo systemctl status mixengine.service -n 0 --no-pager
+sudo systemctl status mixengine.service -n 0 --no-pager --color=always
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart icecast
 echo "## ICECAST SERVICE ##"
-sudo systemctl status icecast2 -n 0 --no-pager
+sudo systemctl status icecast2 -n 0 --no-pager --color=always
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Restart liquidsoap
 echo "## LIQUIDSOAP SERVICE ##"
-sudo systemctl status liquidsoap -n 0 --no-pager
+sudo systemctl status liquidsoap -n 0 --no-pager --color=always
 } | less -r
